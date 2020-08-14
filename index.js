d580efad18c3dcbee0743539277bee5cb050375f
Liz P <parisielizabeth@gmail.com>
2020-08-14 03:01:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+
+function mapToNegativize(sourceArray){
+  let result = [];
+  for (let i = 0; i < sourceArray.length; i++){
+    result.push(sourceArray[i] * -1)
+  }
+  return result;
+}
+
+function mapToNoChange(sourceArray){
+  return sourceArray;
+}
+
+function mapToDouble(sourceArray){
+  let result = [];
+  for (let i = 0; i < sourceArray.length; i++){
+    result.push(sourceArray[i] * 2)
+  }
+  return result;
+}
+
+function mapToSquare(sourceArray){
+  let result = [];
+  for (let i = 0; i < sourceArray.length; i++){
+    result.push(sourceArray[i] * sourceArray[i])
+  }
+  return result;
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+  for (let i = 0; i < sourceArray.length; i++){
+    startingPoint = startingPoint + sourceArray[i];
+  }
+  return startingPoint;
+}
+
+function reduceAllToTrue(sourceArray){
+  let result = true;
+  for (let i = 0; i < sourceArray.length && result == true; i++){
+   result = !!sourceArray[i];
+  }
+  return result;
+}
+
+function reduceAnyToTrue(sourceArray){
+  let result = false;
+  for (let i = 0; i < sourceArray.length && result == false; i++){
+    result = !!sourceArray[i];
+  }
+  return result;
+}
